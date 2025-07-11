1b5051f3b9ec749f83668285b499b341456bf7cd
DavidRobersonN <david.roberson2010@gmail.com>
2025-07-10 21:03:35 -0300

    Este commit adiciona o componente reutilizável PostHeading, que permite definir dinamicamente se o título será um <h1> ou <h2> através da prop . A estilização é aplicada condicionalmente com base na tag informada, utilizando Tailwind CSS e clsx. O conteúdo é envolvido por um Link do Next.js, permitindo navegação interna no site.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,49 @@
-import "tailwindcss";
-import { SpinLoader } from "@/components/Spinloader";
-import { Suspense } from "react";
-import { PostsList } from "@/components/PostsList";
-
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostHeading } from "@/components/PostHeading";
+import { PostsList } from "@/components/PostsList";
+import { SpinLoader } from "@/components/Spinloader";
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
 
-export default async function Home() {
+export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título do post"
+            priority
+          />
+        </Link>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            className="text-slate-600 block text-sm/tight"
+            dateTime="2025-04-20"
+          >
+            20/04/2025 10:00
+          </time>
+
+          <PostHeading as="h1" url="#">
+            Rerum, vel ex? Impedit ullam harum blanditiis
+          </PostHeading>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+            dolorem est dolor porro, doloribus neque, quidem mollitia
+            doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
+            harum blanditiis mollitia?
+          </p>
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
